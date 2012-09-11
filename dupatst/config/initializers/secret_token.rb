4779987676c0e07206744b0c3121ee3299ad19f9
jurekogorek <jerzy.czepiel@gmail.com>
2012-09-11 22:58:55 +0200

    dupa

---
+++ dupatst/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dupatst::Application.config.secret_token = '416b9ec76b43b3c6e9c2ad7f14b90dfa0f4fb76b05125e38aa513131e3736901c36e7e4fe9397b76376a2c09e109c805840e2060e994b86eec050ec448cc472d'
